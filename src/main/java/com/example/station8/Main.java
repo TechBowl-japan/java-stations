43371d944cb504adee0b0855bbfc861f40ae624c
Tamaki Hidetsugu <git@ralph.ms>
2024-03-29 22:15:28 +0900

    Fix 7 and 8

--- src/main/java/com/example/station8/Main.java
+++ src/main/java/com/example/station8/Main.java
@@ -1,9 +1,35 @@
 package com.example.station8;
 
 public class Main {
-    public static void main(String[] args) {
-        int[] itemPrices = { 1200, 2300, 3500, 2500, 1000, 2980, 9800, 4600, 5000, 2000 };
+    public static void question1(int subtotal) {
+        // Station 8 問題(1)
         // ここから
         // ここまで
+    }
+
+    public static void question2(int categoryId) {
+        // Station 8 問題(2)
+        // ここから
+        // ここまで
+    }
+
+    public static void main(String[] args) {
+        System.out.println("問題(1)");
+        System.out.println("合計: 8000円の時");
+        question1(8000);
+        System.out.println("合計: 12000円の時");
+        question1(12000);
+
+        System.out.println("問題(2)");
+        System.out.println("カテゴリID: 1の時");
+        question2(1);
+        System.out.println("カテゴリID: 2の時");
+        question2(2);
+        System.out.println("カテゴリID: 3の時");
+        question2(3);
+        System.out.println("カテゴリID: 99の時");
+        question2(99);
+        System.out.println("カテゴリID: 100の時");
+        question2(100);
     }
 }
