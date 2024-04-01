a6d098a76948ef5f93d7e8ce6b5cd566616df6a9
Àżu <yamane_kota@techbowl.co.jp>
2024-04-01 21:38:34 +0900

    fix: station7, 8のテストを修正

--- src/test/java/com/example/station8/S8.java
+++ src/test/java/com/example/station8/S8.java
@@ -1,11 +1,11 @@
 package com.example.station8;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class S8 {
 
@@ -15,10 +15,55 @@ public class S8 {
         PrintStream out = new PrintStream(baos);
         System.setOut(out);
 
-        Main.main(null);
+        Main.question1(1000);
+        assertEquals("送料800円を追加します\n1800\n", baos.toString());
+        baos.reset();
 
-        String a = baos.toString();
+        Main.question1(9000);
+        assertEquals("送料800円を追加します\n9800\n", baos.toString());
+        baos.reset();
 
-        assertEquals("3488\n", a);
+        Main.question1(9999);
+        assertEquals("送料800円を追加します\n10799\n", baos.toString());
+        baos.reset();
+
+        Main.question1(10000);
+        assertEquals("10000\n", baos.toString());
+        baos.reset();
+
+        Main.question1(20000);
+        assertEquals("20000\n", baos.toString());
+        baos.reset();
+    }
+
+    @Test
+    public void test2() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(baos);
+        System.setOut(out);
+
+        Main.question2(1);
+        assertEquals("食品\n", baos.toString());
+        baos.reset();
+
+        Main.question2(2);
+        assertEquals("家電\n", baos.toString());
+        baos.reset();
+
+        Main.question2(3);
+        assertEquals("家具\n", baos.toString());
+        baos.reset();
+
+        Main.question2(99);
+        assertEquals("その他\n", baos.toString());
+        baos.reset();
+
+        Main.question2(100);
+        assertEquals("", baos.toString());
+        baos.reset();
+
+        Main.question2(4);
+        assertEquals("", baos.toString());
+        baos.reset();
     }
 }
