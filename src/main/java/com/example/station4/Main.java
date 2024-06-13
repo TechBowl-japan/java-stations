3dd75717126883da57c18cf93481d6aeb3150174
TOFU <4416kidr@gmail.com>
2024-06-14 01:23:06 +0900

    clear station4

--- src/main/java/com/example/station4/Main.java
+++ src/main/java/com/example/station4/Main.java
@@ -3,6 +3,8 @@ package com.example.station4;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        System.out.println(18*28 == 24*21); // true
+        System.out.println(3.141592f < 21.9f/7f); // false
         // ここまで
     }
 }
